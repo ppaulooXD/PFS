9dd9b7044f347b46046835aaaf1eed2c2eae2418
ph$ <ph.total12@gmail.com>
2025-03-10 16:55:03 -0300

    feats:abbb

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const expressEjsLayout = require("express-ejs-layouts");
 const rotaLogin = require('./routes/rotaLogin');
 const rotaHome = require("./routes/rotaHome");
+const rotaUsuario = require("./routes/rotaUsuario");
 const app = express();
 const porta = 5000;
 
@@ -13,6 +14,7 @@ app.use(express.urlencoded({extended:true}));
 
 app.use("/", rotaHome);
 app.use("/login", rotaLogin);
+app.use("/usuario", rotaUsuario);
 
 app.listen(porta,function (){
     console.log("Servidor em execução");
